9cca4d7fd351f9bae6c7d3ac414ba3a8a7ef2588
hogehoge <elementrd6@hotmail.com>
2022-11-15 10:47:18 +0900

    first commit

---
+++ Subject.java
@@ -0,0 +1,20 @@
+public class Subject {
+   private String subject;
+   private int score;
+   
+   
+   
+  void setSubject(String subject){
+    this.subject=subject;
+  }
+  String getSubject(){
+    return this.subject;
+  }
+  void setScore(int score){
+    this.score=score;
+  }
+  int getScore(){
+    return this.score;
+  }
+   }
+
---
+++ SubjectApp.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class SubjectApp {
+ public static void main(String[] args) {
+    System.out.print("何科目ありますか?>>");
+    Scanner sc=new Scanner(System.in);
+    int subjectsNum=sc.nextInt();
+    
+Subject subName=new Subject();
+
+for(int i=1;i<subjectsNum;i++){
+for(int j=1;j<subjectsNum;j++){
+System.out.println(i+"科目の名前>>"+subName.getSubject());
+}
+subName.setSubject(sc.nextLine());
+}
+ }
+}
